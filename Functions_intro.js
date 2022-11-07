ff6dccf21dd958a2968af72a0d6650792616c8e1
Anna Titova <amtitova@gmail.com>
2022-11-07 22:27:44 +0100

    For Anna in 2023

---
+++ Functions_intro.js
@@ -0,0 +1,12 @@
+function showMessage(message) {
+  console.log(message);
+}
+
+showMessage("Ты УЖЕ все можешь! Не теряй веры в себя!");
+showMessage("Через год тебе будет смешно, что ты этого боялась!");
+showMessage("Привет тебе, Аня из будущего! Ну как оно?");
+
+function multipleSalary(salary, bonus) {
+  return salary * bonus;
+}
+console.log(multipleSalary(520, 12));
